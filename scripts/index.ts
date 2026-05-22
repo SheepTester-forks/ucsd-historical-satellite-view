89a4d0dd487f3f16362c5ab2a2bca3e2ad040859
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-05-21 21:16:51 -0700

    cache tiles locally to handle interrupted runs

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,10 +1,15 @@
+import { access, constants, mkdir, stat } from "node:fs/promises";
 import backgroundInfoJson from "../facts/backgroundInfo.json" with { type: "json" };
 import backgroundZoomJson from "../facts/backgroundZoom.json" with { type: "json" };
 import {
   getTileUrl,
   parseBackgroundInfo,
   parseBackgroundZoom,
+  type BackgroundInfo,
 } from "./lib/parse.ts";
+import { finished } from "node:stream/promises";
+import { Readable } from "node:stream";
+import { createWriteStream } from "node:fs";
 
 const backgroundInfo = parseBackgroundInfo(backgroundInfoJson).sort((a, b) =>
   a.name.localeCompare(b.name),
@@ -15,6 +20,39 @@ const backgroundZoom = Map.groupBy(
 );
 
 const format = new Intl.NumberFormat();
+
+async function ensureTilePng(
+  layer: BackgroundInfo,
+  zoom: number,
+  x: number,
+  y: number,
+): Promise<string> {
+  const dir = `tiles_png/${layer.name}`;
+  const path = `${dir}/${x.toString().padStart(3, "0")}-${y.toString().padStart(3, "0")}.png`;
+
+  try {
+    await access(path, constants.F_OK);
+    return path;
+  } catch (err) {
+    if (!(err instanceof Error && "code" in err && err.code === "ENOENT")) {
+      throw err;
+    }
+  }
+
+  mkdir(dir, { recursive: true });
+
+  const response = await fetch(getTileUrl(layer, zoom, x, y));
+  if (!response.ok) {
+    console.error(await response.text().catch((err) => err));
+    throw new Error(`HTTP ${response.status}: ${response.url}`);
+  }
+  if (!response.body) {
+    throw new TypeError(`no body: ${response.url}`);
+  }
+
+  await finished(Readable.fromWeb(response.body).pipe(createWriteStream(path)));
+  return path;
+}
 
 let totalTotalSize = 0;
 for (const info of backgroundInfo) {
@@ -38,9 +76,15 @@ for (const info of backgroundInfo) {
     console.warn(`[${info.name}] interesting, zoom=${zoom}`);
   }
 
-  const r = await fetch(getTileUrl(info, zoom, 30, 30));
-  await r.body?.cancel("bruh");
-  const tile0Size = +(r.headers.get("content-length") ?? "");
+  const path = await ensureTilePng(info, zoom, 30, 30);
+  const { size: tile0Size } = await stat(path);
+
+  if (tileCountX >= 100) {
+    console.warn(`[${info.name}] tileCountX=${tileCountX}`);
+  }
+  if (tileCountY >= 100) {
+    console.warn(`[${info.name}] tileCountY=${tileCountY}`);
+  }
 
   const tileCount = tileCountX * tileCountY;
   const totalSize = tileCount * tile0Size;
